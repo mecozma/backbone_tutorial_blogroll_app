78ff620fb1202959e5b899433f83f7e9d1920bae
mecozma <mecozma@gmail.com>
2018-03-10 06:11:45 +0000

    Finished the basic part of the app

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,50 @@ var Blog = Backbone.Model.extend({
 //Backbone Collection
 
 var Blogs = Backbone.Collection.extend({
-    
+    model: new Blog(),
+    tagName: "tr",
+    initiate: function() {
+        this.template = _.template($(".blogs-list-template").html());
+    },
+    render: function() {
+        this.$el.html(this.model.toJSON());
+    }
 });
 
-//Instantiate two Blogs
+//Backbone View for one Blog
+
+var BlogView = Backbone.View.extend({
+    model: new Blog(),
+    tagName: 'tr',
+    initialize: function() {
+        this.template = _.template($('.blog-list-template').html());
+    },
+    render: function() {
+            var attributes = this.model.toJSON();
+            this.$el.html(this.template(attributes));
+            return this;
+        }
+});
+
+//Backbone View for all Blogs
+
+var BlogsView = Backbone.View.extend({
+    model: blogs,
+    el: $(".blogs-list"),
+    initialize: function() {
+        this.model.on('add', this.render, this);
+    },
+    render: function() {
+        var self = this;
+        this.$el.html("");
+        _.each(this.model.toArray(), function(blog) {
+            self.$el.append((new BlogView({model: blog})).render().$el);
+        });
+        return this;
+    }
+});
+
+//Instantiate two Blog Models
 
 var blog1 = new Blog({
     author: "Eduard",
@@ -30,3 +70,19 @@ var blog2 = new Blog({
 //Instantiate a collection
 
 var blogs = new Blogs([blog1, blog2]);
+
+//Instantiate BlogsView
+
+var blogsView = new BlogsView();
+
+$(document).ready(function() {
+    $('.add-blog').on('click', function(){
+        var blog = new Blog({
+            author: $('.author-input').val(),
+            title: $('.title-input').val(),
+            url: $('.url-input').val()
+        });
+        console.log(blog.toJSON());
+        blogs.add(blog);
+    });
+});
